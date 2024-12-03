f5a7c52dd102ff27c1e75772afb647d16bec930a
Katrina Orevillo (She/Her) <58285120+korevillo@users.noreply.github.com>
2024-12-02 18:11:32 -0800

    added tag

--- src/components/Result.js
+++ src/components/Result.js
@@ -26,9 +26,9 @@ function Result({ drink, compatible, description, drinkImage, compatibleImage, r
             <img src={compatibleImage} alt={compatible} />
           </div>
         </div>
-
+        <div className='attribution'><a href="https://instagram.com/notekating">Cozy Cabin Cafe Quiz by IG@notekating</a></div>
       </div>
-
+      
       <button className='quizAgain' onClick={restartQuiz}>Take the Quiz Again &gt;</button>
     </div>
   );
